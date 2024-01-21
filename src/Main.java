a642cb77799a548abb3f47eff4a8e3587cf337d1
Dany.Gen <Gentiev@vk.com>
2024-01-21 13:27:20 +0300

    HW.5

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 //Задача.3
         int year = 2024;
         if (year >= 1584) {
-            if (year % 4 == 0 && year % 100 != 0)
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             {
                 System.out.println(year + " is a leap year.");
             } else {
